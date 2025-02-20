db0c8ea801451a1bcf01e986f421249a020c118e
jerinpeter <jerinpeter@gmail.com>
2025-02-20 01:47:56 -0800

    new update

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const fadeInElements = document.querySelectorAll(".fade-in");
-    const options = { threshold: 0.5 };
-    const observer = new IntersectionObserver(entries => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.style.opacity = 1;
-                entry.target.style.transform = "translateY(0)";
-            }
-        });
-    }, options);
-    fadeInElements.forEach(element => {
-        element.style.opacity = 0;
-        element.style.transform = "translateY(50px)";
-        observer.observe(element);
+// Smooth scrolling for navigation links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
     });
-});
+  });
